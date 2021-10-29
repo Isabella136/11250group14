58081d3668b1955602580c9bb9511edd157971ec
gjrbrosula <gabrielbrosula@gmail.com>
2021-10-28 20:25:51 -0400

    Committed React UI Files

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
+import LineChart from './components/LineChart'
+import ChartSettings from './components/ChartSettings'
 import './App.css';
-import {Line} from 'react-chartjs-2'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to your Dashboard!</h1>
-      <Line
-        data = {{
-          labels:['1','2','3','4','5','6'],
-          datasets:[
-            {
-              label:'# of Votes',
-              data:[12, 19, 3, 5, 2, 3],
-              fill: false,
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgba(255, 99, 232, 0.2)',
-            }
-          ]
-        }}
-        options = {{
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }}
-      ></Line>
-
-
-
-     </div>
+    <div className='rowC'>
+        <ChartSettings />
+        <LineChart />
+    </div>
   );
 }
 
