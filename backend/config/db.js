181f5665ef47f8cf0ed806bf04ef8b8da1497d64
chancethescholar <chanceonyiorah@gmail.com>
2021-11-11 01:35:12 -0500

    Changed database for data input from MongoDB to Cloudant

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const Cloudant = require('@cloudant/cloudant');
+const vcap = require('../../vcap-local.json');
+var cfenv = require("cfenv");
 
 const connectDB = async () => {
   try{
@@ -9,10 +12,24 @@ const connectDB = async () => {
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    //console.log(conn.connection.host); // localhost
+    //console.log(conn.connection.port); // 27017
+    //console.log(conn.connection.name); // myDatabase
+
+
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit();
   }
+
+  //cloudant database connection
+  var cloudant = new Cloudant({ url: 'https://ca4957e3-35db-48ae-b830-5f66e31f42be-bluemix.cloudantnosqldb.appdomain.cloud', plugins: { iamauth: { iamApiKey: 'B5rN-V2C8VsmoVl4iXlGFNwqeZsjVBx36CvRwXz0bPEQ' }  } });
+  cloudant.db.list(function(err, body) {
+    body.forEach(function(db) {
+      console.log(db);
+      });
+  });
 };
 
 module.exports = connectDB;
--- backend/controllers/mockdata_controllers.js
+++ backend/controllers/mockdata_controllers.js
@@ -1,14 +1,37 @@
 const asyncHandler = require('express-async-handler');
 const MockData = require("../models/mockdata_model");
 const User = require("../models/user_model");
+const Cloudant = require('@cloudant/cloudant');
+
+var cloudant;
+var db;
+
+const connectDB = () => {
+  cloudant = new Cloudant({ url: 'https://ca4957e3-35db-48ae-b830-5f66e31f42be-bluemix.cloudantnosqldb.appdomain.cloud', plugins: { iamauth: { iamApiKey: 'B5rN-V2C8VsmoVl4iXlGFNwqeZsjVBx36CvRwXz0bPEQ' }  } });
+  db = cloudant.db.use("data");
+};
 
 const getData = asyncHandler(async (req, res) => {
-  const data = await MockData.find({user: req.user._id});
-  res.json(data);
+  //const data = await MockData.find({user: req.user._id});
+  connectDB();
+
+  var query = {
+    "selector": {
+      "user": req.user._id
+    },
+    "fields": ["_id", "elecConsumption", "elecCost", "waterConsumption", "waterCost", "gasConsumption"]
+  };
+
+  await db.find(query, function(err, data) {
+    console.log('Error:', err);
+    console.log('Data:', data);
+
+    res.json(data.docs);
+  });
 });
 
 const addData = asyncHandler(async (req, res) => {
-  const { elecConsumption, elecCost, waterConsumption,
+  /*const { elecConsumption, elecCost, waterConsumption,
   waterCost, gasConsumption } = req.body;
 
   if (!elecConsumption || !elecCost || !waterConsumption || !waterCost || !gasConsumption) {
@@ -20,7 +43,36 @@ const addData = asyncHandler(async (req, res) => {
     const dataAdded = await data.save();
 
     res.status(201).json(dataAdded);
-  }
+  }*/
+
+  const { elecConsumption, elecCost, waterConsumption,
+  waterCost, gasConsumption } = req.body;
+
+  //var cloudant = new Cloudant({ url: 'https://ca4957e3-35db-48ae-b830-5f66e31f42be-bluemix.cloudantnosqldb.appdomain.cloud', plugins: { iamauth: { iamApiKey: 'B5rN-V2C8VsmoVl4iXlGFNwqeZsjVBx36CvRwXz0bPEQ' }  } });
+  //var db = cloudant.db.use("data");
+
+  connectDB();
+
+  console.log("Creating data document");
+  // specify the id of the document so you can update and delete it later
+  await db.insert({ user: req.user._id, elecConsumption, elecCost, waterConsumption, waterCost, gasConsumption}, function(err, data) {
+    console.log('Error:', err);
+    console.log('Data:', data);
+
+    if(data) {
+      var responseJson =
+      {
+        "elecConsumption": elecConsumption,
+        "elecCost": elecCost,
+        "waterConsumption": waterConsumption,
+        "waterCost": waterCost,
+        "gasConsumption": gasConsumption
+      };
+
+      res.status(200);
+      res.json(responseJson);
+    }
+  });
 });
 
 const getDataById = asyncHandler(async (req, res) => {
