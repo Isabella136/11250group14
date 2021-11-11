37ed292650d1c2e0c2af06b21a596a7133fe3f7e
gjrbrosula <82014893+gabrielbrosula@users.noreply.github.com>
2021-11-11 16:01:07 -0500

    Created dashboard page
    - Fixed so that a date, rather than a timestamp shows on the graph
    - Fixed the settings; no need for checkboxes as you can click on the labels to hide specific lines
    - Added functions that processed data upon retrieval
    - Installed the concurrently and nodemon packages

--- frontend/src/screens/my_graph/my_graph.js
+++ frontend/src/screens/my_graph/my_graph.js
@@ -1,13 +1,13 @@
 import LineChart from '../../components/LineChart';
-import ChartSettings from '../../components/ChartSettings';
+import LineChartCosts from '../../components/LineChartCosts';
 import MainScreen from "../../components/main_screen";
 
 const MyGraph = () => {
   return (
       <MainScreen title="My Graph">
         <div className='rowC'>
-          <ChartSettings />
           <LineChart />
+          <LineChartCosts />
         </div>
       </MainScreen>
   )
