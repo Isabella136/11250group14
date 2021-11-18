e3616550de64a063a5a4ae1f1de8eb89a290d735
Nathalie Bonin <nbonin@ufl.edu>
2021-11-18 03:23:13 -0500

    Change default order of data, added graphs for consumption/cost per month
    Each dataset is ordered based on id, so to order it based on dates, I had to change the id names to the date
    Also added two graphs that showed carbon footprint and cost per month, respectively.

--- frontend/src/screens/my_graph/my_graph.js
+++ frontend/src/screens/my_graph/my_graph.js
@@ -1,13 +1,17 @@
-import LineChart from '../../components/LineChart';
+import LineChartDataEntry from '../../components/LineChartDataEntry';
 import LineChartCosts from '../../components/LineChartCosts';
+import LineChartDataTotal from '../../components/LineChartDataTotal';
+import LineChartCostsTotal from '../../components/LineChartCostsTotal';
 import MainScreen from "../../components/main_screen";
 
 const MyGraph = () => {
   return (
       <MainScreen title="My Graph">
         <div className='rowC'>
-          <LineChart />
+          <LineChartDataEntry />
+          <LineChartDataTotal />
           <LineChartCosts />
+          <LineChartCostsTotal />
         </div>
       </MainScreen>
   )
